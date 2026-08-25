2e8e3cb302bf572309e74ff299aaedfbcdec6658
Gulchehra Xalilova <gulchehra@example.com>
2026-08-25 17:49:35 +0500

    fix final api url fetches

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    // Пробуем вариант с префиксом api и правильными слешами
+    NEXT_PUBLIC_API_URL: 'https://onrender.com',
+  },
+  // Этот блок поможет автоматически убирать двойные слеши, если они возникнут
+  trailingSlash: true, 
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
