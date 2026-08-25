18a49985fe5e02995e6150eedf7b9ebc55f62f8b
Gulchehra Xalilova <gulchehra@example.com>
2026-08-25 17:52:16 +0500

    Force correct render backend production api url

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // Пробуем вариант с префиксом api и правильными слешами
     NEXT_PUBLIC_API_URL: 'https://onrender.com',
   },
-  // Этот блок поможет автоматически убирать двойные слеши, если они возникнут
-  trailingSlash: true, 
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
